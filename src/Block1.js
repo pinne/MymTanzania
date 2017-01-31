13346d99b8f8e05d9271937b8e29835b80572e5d
Paula Berglund <paulalinnea.berglund@gmail.com>
2017-01-31 23:13:20 +0100

    Added Paragraph font, added 'rem' unit that is relative to the root, the htm-element. Some changes to the block styling.

--- src/Block1.js
+++ src/Block1.js
@@ -6,11 +6,14 @@ class Block1 extends Component {
       <section className='block block1'>
         <div className='flex-row flex-align-center block1__content'>
             <div className='oval'>
-              <p className='oval__text'>
+              <div className='oval__text'>
                 MYM STÅR FÖR HOPP OM LIV
-              </p>
+              </div>
             </div>
-            <div className='align-left '>Vårat mål är att skapa, bygga upp och driva olika utvecklingsprojekt som främjar och stärker fattiga och utsatta barn och ungdomars utveckling, utbildning, hälsa, relationer och självständighet. På så vis hoppas vi bidra till ge dem en möjlighet till en hållbar, trygg och hälsosam framtid som de själva kan påverka.</div>
+            <div className='block1__text left-space right-space'>
+              <p>Vårat mål är att skapa, bygga upp och driva olika utvecklingsprojekt som främjar och stärker fattiga och utsatta barn och ungdomars utveckling, utbildning, hälsa, relationer och självständighet. På så vis hoppas vi bidra till ge dem en möjlighet till en hållbar, trygg och hälsosam framtid som de själva kan påverka.</p>
+              <a href="">Läs mer</a>
+          </div>
         </div>
       </section>
     )
--- src/Block2.js
+++ src/Block2.js
@@ -3,8 +3,9 @@ import React, { Component } from 'react';
 class Block2 extends Component {
   render() {
     return (
-      <section className='block block--seconday'>
-        <h2 className='bottom-space'>Även små bidrag är stora. Hur kan du hjälpa?</h2>
+      <section className='block block2 block--seconday'>
+        <p className='block2__titel block2__titel--small'>ÄVEN SMÅ BIDARG ÄR STORA</p>
+        <h2 className='block2__titel bottom-space'>Hur kan du hjälpa?</h2>
         <button className='btn btn--primary bottom-space right-space'>Swisha en gåva</button>
         <button className='btn btn--secondary'>Bli månadsgivare</button>
       </section>
