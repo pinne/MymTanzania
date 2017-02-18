36a69f30f7a132dfd001330b767ae971124af057
Paula Berglund <paulalinnea.berglund@gmail.com>
2017-02-18 18:52:03 +0100

    Payment information block, toggle and close. Also added link to Google Material design icons package

--- src/Block2.js
+++ src/Block2.js
@@ -9,29 +9,23 @@ class Block2 extends Component {
       togglePaymentMonth: false
     }
 
-    // this.textToggleClass = this.toggleProjectInfo.bind(this);
+    this.togglePaymentInfo = this.togglePaymentInfo.bind(this);
+    this.closePaymentInfo = this.closePaymentInfo.bind(this);
   }
 
-  toggleProjectInfo(showInfo) {
+  togglePaymentInfo(showInfo) {
+    showInfo === 'swish' ?   this.setState({togglePaymentSwish: true, togglePaymentMonth: false}) : this.setState({togglePaymentSwish: false, togglePaymentMonth: true}) ;
+  }
 
-    if (showInfo === 'monthly'){
-      this.setState({
-          togglePaymentSwish: false,
-          togglePaymentMonth: true
-      });
-    }
-
-    if (showInfo === 'swish'){
-      this.setState({
-          togglePaymentSwish: true,
-          togglePaymentMonth: false
-      });
-    }
+  closePaymentInfo() {
+    event.preventDefault();
+    this.setState({
+      togglePaymentSwish: false,
+      togglePaymentMonth: false
+    });
   }
 
   render() {
-    const swish = 'swish';
-    const monthly = 'monthly';
     const swishInfo = this.state.togglePaymentSwish ? 'active' : 'inactive';
     const monthlyInfo = this.state.togglePaymentMonth ? 'active' : 'inactive';
 
@@ -40,21 +34,23 @@ class Block2 extends Component {
         <div className='block block2--bg'>
           <p className='block2__text block2__titel--small'>ÄVEN SMÅ BIDARG ÄR STORA</p>
           <h2 className='block2__text bottom-space'>Hur kan du hjälpa?</h2>
-          <button onClick={(e) => {this.toggleProjectInfo(swish)}} className='btn btn--primary bottom-space right-space'>Swisha en gåva</button>
-          <button onClick={(e) => {this.toggleProjectInfo(monthly)}} className='btn btn--secondary'>Bli månadsgivare</button>
+          <button onClick={(e) => this.togglePaymentInfo('swish')} className='block2__btn btn btn--primary block2__buttin'>Swisha en gåva</button>
+          <button onClick={(e) => this.togglePaymentInfo('monthly')} className='block2__btn btn btn--secondary'>Bli månadsgivare</button>
         </div>
         <div className={swishInfo}>
           <div className='block block2__content block2__content--first top-space'>
-            <p className='block2__text'>Vi är hemskt tacksamma för alla gåvor, stora som små. Vill du ge ett bidrag? Sätt in ett valfritt belopp på bankgiro: </p>
-            <h3 className='block2__text'>814-6862</h3>
+            <p className='block2__text'>Swisha en gåva</p>
+            <h3 className='block2__text'>070X-XXX-XXX</h3>
             <p className='block2__text'>TACK!</p>
+            <a onClick={(e) => {this.closePaymentInfo()}} className=' block2__icon'><i className='material-icons'>close</i></a>
           </div>
         </div>
         <div className={monthlyInfo}>
           <div className='block block2__content block2__content--second top-space'>
-            <p className='block2__text'>Bli medlem </p>
-            <h3 className='block2__text'>Bankgiro: 814-6862</h3>
+            <p className='block2__text'>Vi är hemskt tacksamma för alla gåvor, stora som små. Vill du ge ett bidrag? Sätt in ett valfritt belopp på bankgiro:</p>
+            <h3 className='block2__text'>814-6862</h3>
             <p className='block2__text'>TACK!</p>
+            <a onClick={(e) => {this.closePaymentInfo()}} className=' block2__icon'><i className='material-icons'>close</i></a>
           </div>
         </div>
       </section>
