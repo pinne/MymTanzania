4c175f95c52cb57558b83a14d4a867badbeb8911
Paula Berglund <paulalinnea.berglund@gmail.com>
2017-03-14 22:21:26 +0100

    Block2 mobile device, colors and font-size

--- src/Block2.js
+++ src/Block2.js
@@ -33,23 +33,23 @@ class Block2 extends Component {
       <section>
         <div className='block block2 block2--bg'>
           <p className='block2__text block2__titel--small'>ÄVEN SMÅ BIDARG ÄR STORA</p>
-          <h2 className='block2__text bottom-space'>Hur kan du hjälpa?</h2>
+          <h2 className='block2__titel bottom-space'>Hur kan du hjälpa?</h2>
           <button onClick={(e) => this.togglePaymentInfo('swish')} className='block2__btn btn btn--primary block2__buttin'>Swisha en gåva</button>
           <button onClick={(e) => this.togglePaymentInfo('monthly')} className='block2__btn btn btn--secondary'>Bli månadsgivare</button>
         </div>
         <div className={swishInfo}>
           <div className='block block2__content block2__content--first top-space'>
-            <p className='block2__text'>Swisha en gåva</p>
-            <h3 className='block2__text'>070X-XXX-XXX</h3>
-            <p className='block2__text'>TACK!</p>
+            <p className='block2__content-text'>Swisha en gåva</p>
+            <h3 className='block2__content-titel'>070X-XXX-XXX</h3>
+            <p className='block2__content-text'>TACK!</p>
             <a onClick={(e) => {this.closePaymentInfo()}} className=' block2__icon'><i className='material-icons'>close</i></a>
           </div>
         </div>
         <div className={monthlyInfo}>
           <div className='block block2__content block2__content--second top-space'>
-            <p className='block2__text'>Vi är hemskt tacksamma för alla gåvor, stora som små. Vill du ge ett bidrag? Sätt in ett valfritt belopp på bankgiro:</p>
-            <h3 className='block2__text'>814-6862</h3>
-            <p className='block2__text'>TACK!</p>
+            <p className='block2__content-text'>Vi är hemskt tacksamma för alla gåvor, stora som små. Vill du ge ett bidrag? Sätt in ett valfritt belopp på bankgiro:</p>
+            <h3 className='block2__content-titel'>814-6862</h3>
+            <p className='block2__content-text'>TACK!</p>
             <a onClick={(e) => {this.closePaymentInfo()}} className=' block2__icon'><i className='material-icons'>close</i></a>
           </div>
         </div>
--- src/Header.js
+++ src/Header.js
@@ -5,11 +5,15 @@ class Header extends Component {
   render() {
     return (
       <heading>
-        <Top />
+        <div className='image-block__container'>
         <div className='image-block'>
-          <h1 className='image-block__titel left-space right-space'>GÖR EN SKILLNAD</h1>
-          <p className='image-block__text'>Vi är en ideell förening som strävar efter att förbättra livet och villkoren för utsatta barn i Tanzania.</p>
+          <Top />
+          <div className='image-block__content'>
+            <h1 className='image-block__titel left-space right-space'>GÖR EN SKILLNAD</h1>
+            <p className='image-block__text'>Vi är en ideell förening som strävar efter att förbättra livet och villkoren för utsatta barn i Tanzania.</p>
+         </div>
         </div>
+      </div>
     </heading>
     )
   }
