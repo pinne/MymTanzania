fb84cac2514d6c5d3e78072023b3008ff9b5d5b1
Paula Berglund <paulalinnea.berglund@gmail.com>
2017-08-24 22:18:04 +0200

    Block3, added scroll to top or content on toggle.

--- src/block3/Block3.js
+++ src/block3/Block3.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
 import './Block3.css';
 
 class Block3 extends Component {
@@ -17,6 +19,12 @@ class Block3 extends Component {
     this.setState({
       toggled: !this.state.toggled
     });
+
+    this.state.toggled ? this.scroll(this.refs.block3Header) : this.scroll(this.refs.block3Content);
+  }
+  
+  scroll(element) {
+    ReactDOM.findDOMNode(element).scrollIntoView(false);
   }
 
   render() {
@@ -33,12 +41,12 @@ class Block3 extends Component {
               <figure className='block3__image'></figure>
             </div>
             <div>
-              <div className='block3__text-container'>
+              <div ref='block3Header' className='block3__text-container'>
                 <p className='block3__titel-text'>HÄR FOKUSERAR VI EXTRA JUST NU</p>
                 <h2 className='block3__titel'>Projekt: Ukerewe</h2>
                 <p className='block3__ingress'>I byn Hamuyebe på ön Ukerewe i Tanzania växer många idag upp med begränsad möjligheter till utbildning och tillgång till hälsovård. MYM planerar här bygga upp och driva ett utvecklingscenter som vi hoppas kan bidra till en bättre och hållbar framtid för många av barnen som lever här.</p>
               </div>
-              <div className={textToggleClass}>
+              <div ref='block3Content' className={textToggleClass}>
                 <p className='block3__text'>Det projekt som MYM fokuserar främst på just nu handlar om att bygga upp ett center för barnomsorg i byn Hamuyebe på ön Ukerewe i Tanzania. Byn ligger på landsbygden och saknar både el och rinnande vatten och har ingen riktig butik för mat, basvaror eller hälsoartiklar. Tanken är att invånarna i samhället kostnadsfritt ska kunna ta del av verksamheten.</p>
                 <p className='block3__text'>Under 2013 lyckades MYM genom vår lokala organisation MYM Tanzania köpa en bit mark på cirka 4000 kvadratmeter i Hamuyebe. Tomten ligger cirka 20 min cykelväg från den centrala byn Nansio. Den är vackert och strategiskt belägen vid Victoriasjön och bara ett par hundra meter från byns grundskola. Marken är tillräckligt stor för att bygga upp både ett eller ett par hus och lekplats, samt låta bruka en del av marken för lokala stapelvaror så som exempelvis majs, kassava och olika grönsaker.</p>
                 <p className='block3__text'>Målet är att centret ska främja barns både fysiska och psykosociala hälsa. Det ska inspirera till barns utveckling där barnen kan aktivera sig, få stöd, lära sig nya saker, och skapa något att vara stolta över. Exempel på aktiviteter är till exempel läsning och läxläsning, ritning & målning, olika lekar och sporter, musik och instrument, hygien och hälsovård, samt att lära sig praktiska sysslor som att odla, bygga, dreja och sy.</p>
