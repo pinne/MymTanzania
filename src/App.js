b40508a7b075a50953ecb89ae685cc616d040b31
Paula Berglund <paulalinnea.berglund@gmail.com>
2017-08-30 21:49:00 +0200

    comment import on block4

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from './header/Header.js';
 import Block1 from './block1/Block1.js';
 import Block2 from './block2/Block2.js';
 import Block3 from './block3/Block3.js';
-import Block4 from './block4/Block4.js';
+//import Block4 from './block4/Block4.js';
 import Footer from './footer/Footer.js';
 
 
