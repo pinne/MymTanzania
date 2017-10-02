0a81e9484612e9516163921c8e482937db09610c
Paula Berglund <paulalinnea.berglund@gmail.com>
2017-10-02 22:24:42 +0200

    Added news content to Block4 "Puffar" and som styling adjustments

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from './header/Header.js';
 import Block1 from './block1/Block1.js';
 import Block2 from './block2/Block2.js';
 import Block3 from './block3/Block3.js';
-//import Block4 from './block4/Block4.js';
+import Block4 from './block4/Block4.js';
 import Footer from './footer/Footer.js';
 
 
@@ -17,7 +17,7 @@ class App extends Component {
         <Block1 />
         <Block2 />
         <Block3 />
-        {/*<Block4 />*/}
+        <Block4 />
         <Footer />
       </div>
     );
--- src/block4/Block4.js
+++ src/block4/Block4.js
@@ -2,24 +2,24 @@ import React, { Component } from 'react';
 import './Block4.css';
 
 class Block4 extends Component {
-  render() {
-    return (
-      <section className='block block4 flex-row'>
-          <div className='block4__color-box block4__color-box--light'>
-            <h3 className='block4__color-box-titel'>NU LANSERAR VI VÅR NYA HEMSIDA</h3>
-            <p>Äntligen har det blivit dags att lansera den nya hemsidan.  Arbetet har varit en stor del av den nya fas vi nu går in i. Nu kör vi!</p>
-          </div>
-          <div className='block4__color-box block4__color-box--medium'>
-            <h3 className='block4__color-box-titel'>NU LANSERAR VI VÅR NYA HEMSIDA</h3>
-            <p>Äntligen har det blivit dags att lansera den nya hemsidan.  Arbetet har varit en stor del av den nya fas vi nu går in i. Nu kör vi!</p>
-          </div>
-          <div className='block4__color-box block4__color-box--dark'>
-            <h3 className='block4__color-box-titel'>NU LANSERAR VI VÅR NYA HEMSIDA</h3>
-            <p>Äntligen har det blivit dags att lansera den nya hemsidan.  Arbetet har varit en stor del av den nya fas vi nu går in i. Nu kör vi!</p>
-          </div>
-      </section>
-    )
-  }
+    render() {
+        return (
+            <section className='block block4'>
+                <div className='block4__color-box block4__color-box--light'>
+                    <h3 className='block4__color-box-titel'>SEPTEMBER</h3> 
+                    <p>Den 12 september kickar vi igång tävlingen med Ingenjörer utan gränser för att rita vårt kommande center i byn Hamuyebe. Tävlingen är öppen för arkitekt - och ingenjörsstudenter på Lunds Universitet.Nu kör vi!</p>
+                </div> 
+                <div className='block4__color-box block4__color-box--medium'>
+                    <h3 className='block4__color-box-titel'>AUGUSTI</h3>
+                    <p>MYM har anlitat en lantmätare som har ritat en topografisk karta över vår tomt i byn Hamuyebe. Denna karta har kombinerats med kartan som Ingenjörer utan gränsers ritade när de besökte oss, och kommer ligga till grund för ritningarna av vårt kommande center.</p>
+                </div>
+                <div className='block4__color-box block4__color-box--dark'>
+                    <h3 className='block4__color-box-titel'>JULI</h3>
+                    <p>Ingenjörer utan gränser har besökt Ukerewe och tomten i Hamuyebe där vi ska bygga upp vårt center för barnomsorg.Jätteroligt tycker vi på MYM, och tack så mycket för att ni har filmat, tagit bilder samt hjälpt oss att rita en karta över tomten.</p>
+                </div>
+            </section>
+        )
+    }
 }
 
 export default Block4;
