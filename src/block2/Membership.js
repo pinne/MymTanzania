f0fe3fc4bc9869c9d451cb8952077bc2c2300821
Simon Kers <skers@kth.se>
2017-11-14 20:49:48 +0100

    Adding title to iframe

--- src/block2/Membership.js
+++ src/block2/Membership.js
@@ -6,6 +6,7 @@ export default () => {
     <div className='membership__content'>
       <iframe
         src='https://docs.google.com/forms/d/e/1FAIpQLSeygsRRVfjyVOlMEwUkLrr-UWU2YlIE9tI0_8V4idnH7lsEVA/viewform?embedded=true'
+        title='Signup form'
         height='1050'
         width='100%'
         frameBorder='0'
