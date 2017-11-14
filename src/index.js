067cadb6d9f68eb77eab5f065ed075c9521da68b
Simon Kers <skers@kth.se>
2017-11-14 21:19:08 +0100

    Google analytics support

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactGA from 'react-ga';
 import App from './App';
 import './index.css';
+
+ReactGA.initialize('UA-819513-7');
 
 ReactDOM.render(
   <App />,
