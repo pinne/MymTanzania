30bb2ed0b41256c47c71fc315659c7d39f93b3e4
Paula Berglund <paulalinnea.berglund@gmail.com>
2017-11-22 22:42:53 +0100

    Updated email adress in the footer. Text adjustments in the news block

--- src/block4/Block4.js
+++ src/block4/Block4.js
@@ -7,7 +7,7 @@ class Block4 extends Component {
             <section className='block block4'>
                 <div className='block4__color-box block4__color-box--light'>
                     <h3 className='block4__color-box-titel'>SEPTEMBER</h3> 
-                    <p>Den 12 september kickar vi igång tävlingen med Ingenjörer utan gränser för att rita vårt kommande center i byn Hamuyebe. Tävlingen är öppen för arkitekt - och ingenjörsstudenter på Lunds Universitet.Nu kör vi!</p>
+                    <p>Den 12 september kickar vi igång tävlingen med Ingenjörer utan gränser för att rita vårt kommande center i byn Hamuyebe. Tävlingen är öppen för arkitekt - och ingenjörsstudenter på Lunds Universitet. Nu kör vi!</p>
                 </div> 
                 <div className='block4__color-box block4__color-box--medium'>
                     <h3 className='block4__color-box-titel'>AUGUSTI</h3>
@@ -15,7 +15,7 @@ class Block4 extends Component {
                 </div>
                 <div className='block4__color-box block4__color-box--dark'>
                     <h3 className='block4__color-box-titel'>JULI</h3>
-                    <p>Ingenjörer utan gränser har besökt Ukerewe och tomten i Hamuyebe där vi ska bygga upp vårt center för barnomsorg.Jätteroligt tycker vi på MYM, och tack så mycket för att ni har filmat, tagit bilder samt hjälpt oss att rita en karta över tomten.</p>
+                    <p>Ingenjörer utan gränser har besökt Ukerewe och tomten i Hamuyebe där vi ska bygga upp vårt center för barnomsorg. Jätteroligt tycker vi på MYM, och tack så mycket för att ni har filmat, tagit bilder samt hjälpt oss att rita en karta över tomten.</p>
                 </div>
             </section>
         )
