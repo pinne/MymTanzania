8639a4c3024386c419135dd4c231e31791d5d9e6
Paula Berglund <paulalinnea.berglund@gmail.com>
2018-01-25 18:32:45 +0100

    Instagram feed for React
    - Added react-instafeed
    - Added styling and markup

--- src/index.js
+++ src/index.js
@@ -4,9 +4,20 @@ import ReactGA from 'react-ga';
 import App from './App';
 import './index.css';
 
+const contentful = require('contentful')
+contentful.createClient({
+  // This is the space ID. A space is like a project folder in Contentful terms
+  space: '22kovaj11tjg',
+  // This is the access token for this space. Normally you get both ID and the token in the Contentful web app
+  accessToken: '752b4d09e0a3c9cc42baa37b0f0f36ac262a6cc5ec75624aa85af4b895d626c9'
+});
+// This API call will request an entry with the specified ID from the space defined at the top, using a space-specific access token.
+// client.getEntry('752b4d09e0a3c9cc42baa37b0f0f36ac262a6cc5ec75624aa85af4b895d626c9')
+//   .then((entry) => console.log(entry));
+
 ReactGA.initialize('UA-819513-7');
 
-ReactDOM.render(
-  <App />,
+ReactDOM.render( <
+  App / > ,
   document.getElementById('root')
 );
