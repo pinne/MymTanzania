8d677e602590fdd076b3f287ea191217fb841bb8
Paula Berglund <paulalinnea.berglund@gmail.com>
2018-04-03 19:55:02 +0200

    Instafeed fix

--- src/block4/Block4.js
+++ src/block4/Block4.js
@@ -14,7 +14,7 @@ class Block4 extends Component {
                         resolution='standard_resolution'
                         sortBy='most-recent'
                         target={instafeedTarget}
-                        template='<a class="instafeedTarget__link" href="{{link}}" target="_blank" id="{{id}}"><img class="instafeedTarget__image" src="{{image}}" /><span class="instafeedTarget__caption">{{model.short_caption}}</span></a>'
+                        template='<a class="instafeedTarget__link" href="{{link}}" target="_blank" id="{{id}}"><img class="instafeedTarget__image" src="{{image}}" /><div class="instafeedTarget__caption">{{model.short_caption}}</div></a>'
                         userId='5908742622'
                         clientId='ca26c24e91ee4a75855afb4b5e3f2392'
                         accessToken='5908742622.1677ed0.27ca5c5f31754aa4b1419f8f1bd468b6'/>
