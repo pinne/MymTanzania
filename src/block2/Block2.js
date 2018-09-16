215bdffb206614c44940b0590d82bb1bf555b09d
Paula Berglund <paulalinnea.berglund@gmail.com>
2018-09-16 16:24:33 +0200

    Swish

--- src/block2/Block2.js
+++ src/block2/Block2.js
@@ -42,6 +42,7 @@ class Block2 extends Component {
         <div className={swishInfo}>
           <div className='block block2__content block2__content--first top-space'>
             <p className='block2__content-text'>Fantastiskt att du vill bidra till vårt värdefulla arbete! <br/>Om du vill att bidraget ska gå till ett specifikt projekt uppge detta i meddelandefältet när du gör din betalning.</p>
+            <h3 className='block2__content-titel'>Swish: 1233 508 116</h3>
             <h3 className='block2__content-titel'>Bankgiro: 814-6862</h3>
             <p className='block2__content-text'>Tack för din hjälp!</p>
             <a onClick={(e) => {this.closePaymentInfo(e)}} className=' block2__icon'><i className='material-icons'>close</i></a>
