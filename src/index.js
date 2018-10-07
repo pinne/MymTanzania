0ecf404bb6a7ce3e649c03140d873c5776b38423
Simon Kers <skers@kth.se>
2018-10-07 14:35:15 +0200

    Simpler handilng of Analytics activation

--- src/index.js
+++ src/index.js
@@ -5,6 +5,12 @@ import App from './App';
 import './index.css';
 
 ReactGA.initialize('UA-819513-7');
+const deployRegex = /^https?:\/\/www\.mymtanzania\.se/
+const isDeployed = document.location.href.match(deployRegex)
+if (isDeployed) {
+  ReactGA.initialize('UA-819513-7');
+  ReactGA.pageview(window.location.pathname + window.location.search);
+}
 
 ReactDOM.render(
   <App />,
